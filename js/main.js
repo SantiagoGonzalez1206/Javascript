bac65265b8c7f54d761de387b68f51ba1355489b
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-05-27 09:00:11 -0500

    inicio de js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let text = "Hola soy santi"
+let number = 17
+let bolean = false
+let notdefined
+
+console.log(text)
+console.log(number)
+console.log(bolean)
+console.log(notdefined)
+
+console.log(typeof text)
+console.log(typeof number);
+console.log(typeof bolean); 
+console.log(typeof notDefined);
+
+if(true){
+    let nombre="Santiago"
+    console.log(nombre);
+}
